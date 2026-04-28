137fecfe0cc61f39cdc5a3d2575cbcd6b6f1f50a
AlexisCh <temporalxav@outlook.es>
2026-04-28 10:15:19 -0500

    feat: add Product repository class

---
+++ inventarioMVC/src/main/java/com/AS/inventarioMVC/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.AS.inventarioMVC.repository;
+
+/**
+ *
+ * @author DELL
+ */
+public class ProductRepository {
+    
+}
