8b87ebdbac5110bc74a639e1c0443b644040606a
AlexisCh <temporalxav@outlook.es>
2026-04-28 07:09:58 -0500

    feat: initial directory structure

---
+++ InventarioEspaguetiCLI/src/main/java/com/mycompany/inventarioespagueticli/InventarioEspaguetiCLI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventarioespagueticli;
+
+/**
+ *
+ * @author DELL
+ */
+public class InventarioEspaguetiCLI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
