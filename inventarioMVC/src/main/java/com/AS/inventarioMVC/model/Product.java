f4cb2e3c3448ee3aa999cfc0848854de0efe474a
AlexisCh <temporalxav@outlook.es>
2026-04-28 10:02:42 -0500

    feat: add Product model class

---
+++ inventarioMVC/src/main/java/com/AS/inventarioMVC/model/Product.java
@@ -0,0 +1,64 @@
+
+package com.AS.inventarioMVC.model;
+
+public class Product {
+    private String name;
+    private int quantity;
+    private double price;
+    private String category;
+    private String supplier;
+
+    public Product() {
+    }
+
+    public Product(String name, int quantity, double price, String category, String supplier) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.category = category;
+        this.supplier = supplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    
+    
+}
